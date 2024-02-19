dfd84767f4f13496e164edbe6e7a9d3e95098732
ATS <lucas.almida.da.silva@gmail.com>
2024-02-19 00:03:39 -0400

    Update astro.config.mjs

--- FRONTEND/astro.config.mjs
+++ FRONTEND/astro.config.mjs
@@ -6,7 +6,7 @@ import vercel from "@astrojs/vercel/serverless";
 // https://astro.build/config
 export default defineConfig({
   integrations: [react()],
-  output: 'static',
+  output: 'hybrid',
   adapter: vercel({
     imageService: true,
     webAnalytics: {
